a72dc06fc6aac9be1058a59eea4a053ed045575a
Haftomteame <159965039+Haftomteame@users.noreply.github.com>
2024-10-03 11:56:48 +0200

    Add files via upload

---
+++ mot_de_pass.ts
@@ -0,0 +1,36 @@
+function generetMotDePass(length: number ,includeNumbers: boolean, includeSymbols: boolean, includeUppercase: boolean): string
+{
+
+if( length > 10 && length < 25 ){
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '@#$%^&*()_+~';  
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    
+    let characters = lowercase[Math.floor(Math.random() * lowercase.length)];  
+    if (includeNumbers) {
+        characters += numbers[Math.floor(Math.random() * numbers.length)];
+    }
+    if (includeSymbols) {
+        characters += symbols[Math.floor(Math.random() * symbols.length)];     // Symbole
+    }
+    if (includeUppercase) {
+        characters += uppercase[Math.floor(Math.random() * uppercase.length)]; // Majuscule
+    }   
+
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+      return password;
+}
+return "la longueur de mot de pass doit etre entre 12 et 25";
+}
+
+const password = generetMotDePass(17, true, true, true);
+console.log(password);
+ 
